dc8fb56ec1a068443451cde91b70f6c0db758c09
amirali irvany <dev.amirali.irvany@gmail.com>
2024-03-18 21:56:12 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
 from distutils.core import setup
 from setuptools import find_packages
-import os
-
-
-current_directory = os.path.dirname(os.path.abspath(__file__))
-try:
-    with open(os.path.join(current_directory, 'README.md'), encoding='utf-8') as f:
-        long_description = f.read()
-except Exception:
-    long_description = 'Translation of various texts without restrictions and without the need for authentication, token, access key, etc'
 
 
 setup(
@@ -17,12 +8,11 @@ setup(
     version='0.0.1',
     license='MIT',
     description='A Python package to use in translating texts based on api',
-    long_description=long_description,
+    long_description=open('README.md', encoding='utf-8').read(),
     long_description_context_type='text/markdown',
     author='amirali irvany',
     author_email='dev.amirali.irvany@gmail.com',
-    url='https://metect.github.io',
-    download_url='https://github.com/metect/itranslator',
+    url='https://github.com/metect/itranslator',
     keywords=['translate', 'translator', 'google', 'google-translate', 'free-translator'],
     install_requires=['requests', 'user_agent'],
 )
