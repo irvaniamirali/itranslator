2034a7a62515c3d1c511632c95d7847e6d99d896
amirali irvany <dev.amirali.irvany@gmail.com>
2024-04-09 17:24:53 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,32 @@
-from distutils.core import setup
-from setuptools import find_packages
+from setuptools import setup, find_packages
 
+
+with open('README.md', encoding='UTF-8') as file:
+    readme = file.read()
 
 setup(
     name='itranslator',
-    packages=find_packages('.'),
     version='0.0.2',
-    license='MIT',
-    description='A Python package to use in translating texts based on api',
-    long_description=open('README.md', encoding='utf-8').read(),
-    long_description_context_type='text/markdown',
     author='amirali irvany',
     author_email='dev.amirali.irvany@gmail.com',
+    description='A Python package to use in translating texts based on api',
+    keywords=['translate', 'translator', 'google', 'google-translate', 'free-translator', 'asyncio'],
+    long_description=readme,
+    python_requires="~=3.7",
+    long_description_content_type='text/markdown',
     url='https://github.com/metect/itranslator',
-    keywords=['translate', 'translator', 'google', 'google-translate', 'free-translator'],
-    install_requires=['requests', 'user_agent'],
+    packages=find_packages(),
+    exclude_package_data={'': ['*.pyc', '*__pycache__*']},
+    install_requires=requirements,
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
