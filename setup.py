b6949f8425885fd9650882cd682b4c8fa6767a16
AmirAli Irvani <irvaniamirali@proton.me>
2024-08-19 00:10:54 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,18 +6,18 @@ with open('README.md', encoding='UTF-8') as file:
 
 setup(
     name='itranslator',
-    version='1.0.0',
-    author='amirali irvany',
-    author_email='dev.amirali.irvany@gmail.com',
+    version='2.0',
+    author='AmirAli Irvani',
+    author_email='social.irvaniamirali@gmail.com',
     description='A Python package to use in translating texts based on api',
     keywords=['translate', 'translator', 'google', 'google-translate', 'free-translator', 'asyncio'],
     long_description=readme,
     python_requires="~=3.7",
     long_description_content_type='text/markdown',
-    url='https://github.com/metect/itranslator',
+    url='https://github.com/irvaniamirali/itranslator',
     packages=find_packages(),
     exclude_package_data={'': ['*.pyc', '*__pycache__*']},
-    install_requires=['requests', 'user_agent'],
+    install_requires=['httpx', 'user_agent'],
     classifiers=[
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
